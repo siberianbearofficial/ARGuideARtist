8fd50428c9a931f663f6876d4d00a1e25d1f78d4
siberianbearofficial <alexeyorlov65@gmail.com>
2022-03-09 07:35:51 +0300

    nav routes

---
+++ data/navigation_routs.py
@@ -0,0 +1,10 @@
+import sqlalchemy
+from sqlalchemy_serializer import SerializerMixin
+from data.db_session import SqlAlchemyBase
+
+
+class Navigations_rout(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'navigations_rout'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    routs_id = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    navigation = sqlalchemy.Column(sqlalchemy.String, nullable=False)
